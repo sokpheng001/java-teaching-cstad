313136947586b20b65989b2405678477359b3000
sokpheng001 <kimchansokpheng123@gmail.com>
2024-01-28 11:37:33 +0700

    Pulled from office

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import java.io.*;
 
-public class Main{
+
+record Person(String id) implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L; // Helps in version control during deserialization
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                '}';
+    }
+}
+
+public class Main {
+    static void writingTextToFile(String content){
+        try(FileOutputStream fileOutputStream = new FileOutputStream("Test.txt")){
+            byte [] bytes = content.getBytes();
+            fileOutputStream.write(bytes);
+        }catch (IOException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
     public static void main(String[] args) {
-        //
-
+        writingTextToFile("Hello, It's DevOps class");
     }
 }
