4357298de4d8f3fba4e53bad2df10877d99b19e9
sokpheng001 <kimchansokpheng123@gmail.com>
2024-02-04 20:54:02 +0700

    Symbolic Link test

--- src/fileIOvol2/File1.java
+++ src/fileIOvol2/File1.java
@@ -1,21 +1,12 @@
 package fileIOvol2;
 
+import globalVariable.GlobalVariable;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class File1 {
     public static void main(String[] args) {
-        Path file = Paths.get("Test.csv");
-        Path destination = Paths.get("C:\\Users\\burit\\OneDrive\\Desktop\\Test.csv");
-        System.out.println(destination);
-        try{
-            Files.copy(file, destination);
-            System.out.println(STR."File \{file.getFileName()} has been copied successfully.");
-        }catch (IOException ioException){
-            System.out.println(STR."Error: \{ioException.getLocalizedMessage()}");
-        }
+        Path targetFile = Paths.get(GlobalVariable.OUT_PUT_FILE_PATH.getPath()).resolve("");
     }
 }
--- src/javaFileIO/testPathInteterface/Main.java
+++ src/javaFileIO/testPathInteterface/Main.java
@@ -1,6 +1,5 @@
 package javaFileIO.testPathInteterface;
 
-import globalVariable.GlobalVariable;
 
 public class Main {
     public static void main(String[] args) {
